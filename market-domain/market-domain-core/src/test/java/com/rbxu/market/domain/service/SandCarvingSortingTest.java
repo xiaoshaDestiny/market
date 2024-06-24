eb4a652d80d2534dcc617be2d17dbc46e084d4b1
rbxu <xuruibiao@ynlcgroup.com>
2024-06-24 15:49:11 +0800

    面条排序

--- market-domain/market-domain-core/src/test/java/com/rbxu/market/domain/service/SandCarvingSortingTest.java
+++ market-domain/market-domain-core/src/test/java/com/rbxu/market/domain/service/SandCarvingSortingTest.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +12,44 @@ import java.util.List;
  */
 @Slf4j
 public class SandCarvingSortingTest {
+
+    /**
+     * 面条排序：将输入分别对应到不同长度的面条上，每根面条的长度即为对应的数字的大小。
+     * 比如，对于[1, 4, 2, 8, 9]这个输入，则分别做出长度为1cm、4cm、2cm、8cm、9cm的面条。
+     * 然后，将这些面条的一头对其，用手抓住，另一头向下。然后慢慢地将手向下垂直下降，
+     * 第一个触碰到桌面的面条对应的数字则为最大的数字，第二个触碰到的就是第二大的，依次类推。
+     *
+     * (1) 搞不定负数
+     * (2) 只能从大到小
+     */
+    @Test
+    public void testNoodleSort() {
+        List<Integer> waitSort = Lists.newArrayList(1, 4, 2, 8, 9);
+        log.info("before: {}", waitSort);
+
+        List<List<String>> noodles = Lists.newArrayList();
+        for (Integer x : waitSort) {
+            List<String> aNoodle = Lists.newArrayList();
+            for (int i = 0; i <= x-1 ; i++) {
+                aNoodle.add("面的一部分");
+            }
+            noodles.add(aNoodle);
+        }
+
+        List<Integer> afterSort = Lists.newArrayList();
+
+        // 场地高度100
+        int placeHeight = 100;
+        for (int i = placeHeight; i >= 0 ; i--) {
+            for (List<String> noodle : noodles) {
+                if (noodle.size() == i) {
+                    afterSort.add(noodle.size());
+                }
+            }
+        }
+
+        log.info("after: {}", afterSort);
+    }
 
     /**
      * 猴子排序法：进来就检查是不是有序的，不是的话随机打乱，再检查
